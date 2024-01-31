3838783633cb00cf57a50786e3c3b3f187fde9a9
ProfoundlyParker <parkerramey@yahoo.com>
2024-01-31 14:19:12 -0500

    Adding initial files + Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { IoPartlySunny } from "react-icons/io5";
+
+type Props = {
+
+}
+
+export const Navbar: React.FC<Props> = (props) => {
+    return (
+      <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+        <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+            <p className='flex items-center justify-center gap-2'>
+                <h2 className='text-gray-500 text-3xl'>Weather</h2>
+                <IoPartlySunny className='text-4xl mt-1 text-blue-400' />
+            </p>
+        </div>
+      </nav>
+    );
+};
